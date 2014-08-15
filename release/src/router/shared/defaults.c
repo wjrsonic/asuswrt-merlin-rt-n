8f034a8cca81b8a2a859863f614fc41cf59c0912
AndreyPopovNew <admin@csa.dp.ua>
2014-08-15 18:02:53 +0300

    add choose defaultroute for vpn client and support when non-default routing (rebase 378.3913) reworked in 376.3677!!!

--- release/src/router/shared/defaults.c
+++ release/src/router/shared/defaults.c
@@ -1709,6 +1709,12 @@ struct nvram_tuple router_defaults[] = {
 	{ "vpnc_connect_row", ""},
 	{ "vpnc_auto_conn", ""},
 	{ "vpnc_pptp_options_x_list", ""},
+	{ "vpnc_defgw", ""},
+	{ "vpnc_defaultroute", ""},
+	{ "vpnc_addroute", ""},
+	{ "vpnc_addroute_net", ""},
+	{ "vpnc_addroute_mask", ""},
+	{ "vpnc_nodefgw_net", ""},
 #endif
 
 #if defined(RTCONFIG_PPTPD) || defined(RTCONFIG_ACCEL_PPTPD) || defined(RTCONFIG_OPENVPN)
