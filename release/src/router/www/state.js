ec9b91e343b628e8366a515e0aae36c3d4498aa0
AndreyPopovNew <admin@csa.dp.ua>
2014-08-15 20:19:28 +0300

    return to ASUS version numberation in webui (rebase 376.3677)

--- release/src/router/www/state.js
+++ release/src/router/www/state.js
@@ -549,7 +549,11 @@ function remove_url(){
 
 	if(downsize_4m_support) {
 		remove_menu_item("Main_ConnStatus_Content.asp");
-		remove_menu_item("Main_TrafficMonitor_realtime.asp");		
+		remove_menu_item("Main_TrafficMonitor_realtime.asp");
+		remove_menu_item("Tools_Sysinfo.asp");
+		remove_menu_item("Advanced_Wireless_Survey.asp");
+		remove_menu_item("Advanced_Feedback.asp");
+		remove_menu_item("ParentalControl.asp");	
 	}
 	
 	if(downsize_8m_support) {
@@ -721,9 +725,9 @@ function remove_url(){
 		remove_menu_item("AdaptiveQoS_ROG.asp");
 	}
 
-//	if(!cooler_support){
-//		remove_menu_item("Advanced_PerformanceTuning_Content.asp");
-//	}
+	if(!cooler_support){
+		remove_menu_item("Advanced_PerformanceTuning_Content.asp");
+	}
 
 	if(!ParentalCtrl2_support && !yadns_support && !dnsfilter_support){
 		menuL1_title[4] = "";
@@ -1890,17 +1894,18 @@ function show_top_status(){
 	var swpjverno = '<% nvram_get("swpjverno"); %>';
 	var buildno = '<% nvram_get("buildno"); %>';
 	var firmver = '<% nvram_get("firmver"); %>'
-	var extendno = '<% nvram_get("extendno"); %>';
+		var extendno = '<% nvram_get("extendno"); %>';
+		if(extendno == "") extendno="0";
 
-	if(swpjverno == ''){
-		if ((extendno == "") || (extendno == "0"))
-			showtext($("firmver"), buildno);
+  if(swpjverno == ''){
+		if(swisscom_support)
+			showtext($("firmver"), firmver + "." + buildno + '_' + extendno.split("-g")[0] + '_swisscom' );
 		else
-			showtext($("firmver"), buildno + '_' + extendno.split("-g")[0]);
+			showtext($("firmver"), firmver + "." + buildno + '_' + extendno.split("-g")[0]);
 	}
-	else{
+  else{
 		showtext($("firmver"), swpjverno + '_' + extendno);
-	}
+  }
 
 
 	// no_op_mode
