5d8cad5b78e8ddf4e9de2c8ca7a12c7790ec714a
AndreyPopovNew <admin@csa.dp.ua>
2014-09-20 16:20:49 +0300

    change Ukrainian string (rebase 378.3913)

--- release/src/router/www/state.js
+++ release/src/router/www/state.js
@@ -2316,7 +2316,7 @@ function show_selected_language(){
 			break;
 		}	
 		case 'UK':{
-			$('selected_lang').innerHTML = "Український";
+			$('selected_lang').innerHTML = "Українська";
 			break;
 		}		
 	}
