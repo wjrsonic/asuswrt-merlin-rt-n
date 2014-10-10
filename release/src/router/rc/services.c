8b7ee3e8e8af8dade53d566816ff845acc8020be
AndreyPopovNew <admin@csa.dp.ua>
2014-10-10 20:19:42 +0300

    when no usb storage ftp not start/stop (rebase 376.3677)

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -4642,7 +4642,7 @@ check_ddr_done:
 		/* for security concern, even if you stop ftp daemon, it is better to restart firewall to clean FTP port: 21. */
 		start_firewall(wan_primary_ifunit(), 0);
 	}
-#endif
+
 	else if (strcmp(script, "ftpd_force") == 0)
 	{
 		nvram_set("st_ftp_force_mode", nvram_safe_get("st_ftp_mode"));
@@ -4654,6 +4654,7 @@ check_ddr_done:
 		/* for security concern, even if you stop ftp daemon, it is better to restart firewall to clean FTP port: 21. */
 		start_firewall(wan_primary_ifunit(), 0);
 	}
+#endif
 #ifdef RTCONFIG_SAMBASRV
 	else if (strcmp(script, "samba") == 0)
 	{
