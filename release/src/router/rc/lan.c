2fc8f503afa203dae121afe4a5baf36863839b0a
AndreyPopovNew <admin@csa.dp.ua>
2014-10-15 18:44:43 +0300

    disable DNSMQ filter for 4MB routers (rebase 378.3873)
    fix typo in dnsmq function (peplace void to int) post 378.3762 rebase

--- release/src/router/rc/lan.c
+++ release/src/router/rc/lan.c
@@ -2061,6 +2061,8 @@ gmac3_no_swbr:
 	free(lan_ifname);
 
 #ifdef RTCONFIG_WIRELESSREPEATER
+// no dnsmq for 4MB
+#ifndef RTCONFIG_4M_SFP
 	if(get_model() == MODEL_APN12HP &&
 		nvram_get_int("sw_mode") == SW_MODE_AP) {
 		// When CONNECTED, need to redirect 10.0.0.1
@@ -2073,6 +2075,7 @@ gmac3_no_swbr:
 		f_write_string("/proc/net/dnsmqctrl", domain_mapping, 0, 0);
 		start_nat_rules();
 	}
+#endif
 #endif
 
 #ifdef RTCONFIG_QTN
@@ -3336,6 +3339,8 @@ lan_up(char *lan_ifname)
 #endif
 
 #ifdef RTCONFIG_WIRELESSREPEATER
+// no dnsmq for 4MB
+#ifndef RTCONFIG_4M_SFP
 	if(get_model() == MODEL_APN12HP &&
 		nvram_get_int("sw_mode") == SW_MODE_AP) {
 		repeater_nat_setting();
@@ -3346,6 +3351,7 @@ lan_up(char *lan_ifname)
 		f_write_string("/proc/net/dnsmqctrl", domain_mapping, 0, 0);
 		start_nat_rules();
 	}
+#endif
 #endif
 
 #ifdef RTCONFIG_USB
@@ -4044,6 +4050,8 @@ void restart_wireless(void)
 #endif
 
 #ifdef RTCONFIG_WIRELESSREPEATER
+// no dnsmq for 4MB
+#ifndef RTCONFIG_4M_SFP
 	// when wlc_mode = 0 & wlc_state = WLC_STATE_CONNECTED, don't notify wanduck yet.
 	// when wlc_mode = 1 & wlc_state = WLC_STATE_CONNECTED, need to notify wanduck.
 	// When wlc_mode = 1 & lan_up, need to set wlc_state be WLC_STATE_CONNECTED always.
@@ -4068,6 +4076,7 @@ void restart_wireless(void)
 		f_write_string("/proc/net/dnsmqctrl", domain_mapping, 0, 0);
 		start_nat_rules();
 	}
+#endif
 #endif
 
 #ifdef RTCONFIG_QCA
@@ -4170,6 +4179,8 @@ void restart_wireless_wps(void)
 #endif
 
 #ifdef RTCONFIG_WIRELESSREPEATER
+// no dnsmq for 4MB
+#ifndef RTCONFIG_4M_SFP
 	// when wlc_mode = 0 & wlc_state = WLC_STATE_CONNECTED, don't notify wanduck yet.
 	// when wlc_mode = 1 & wlc_state = WLC_STATE_CONNECTED, need to notify wanduck.
 	// When wlc_mode = 1 & lan_up, need to set wlc_state be WLC_STATE_CONNECTED always.
@@ -4194,6 +4205,7 @@ void restart_wireless_wps(void)
 		f_write_string("/proc/net/dnsmqctrl", domain_mapping, 0, 0);
 		start_nat_rules();
 	}
+#endif
 #endif
 
 	nvram_set_int("wlready", 1);
